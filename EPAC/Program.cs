284d7095b2e4e012215ef89d86d12f9cc67a0078
Javier García Menéndez <javier.a.garcia@avanade.com>
2025-08-21 12:54:07 +0200

    Add EPAC baseline with .gitignore

---
+++ EPAC/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+git remote add origin https://github.com/jagamesa/EPAC.git
+git branch -M main
+git push -u origin main
